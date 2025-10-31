4240431bf02f3139becf462594d106cd42c0133e
Gulnaz Sheikh <gulnazsheikh.dev@gmail.com>
2025-10-31 14:24:39 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,21 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-const isProd = process.env.NODE_ENV === "production";
-
-console.log(">>> NODE_ENV:", process.env.NODE_ENV);
-console.log(">>> BASE PATH:", isProd ? "/sign-in" : "");
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? "/sign-in" : "",
-  assetPrefix: isProd ? "/sign-in/" : "",
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/sign-in" : "",
-  },
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
   sassOptions: {
     includePaths: [path.join(__dirname, "node_modules")],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/sign-in",
+        permanent: false, // temporary redirect so you can still adjust it later
+      },
+    ];
   },
 };
 
