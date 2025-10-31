4d00608178dd2b96bf65a80e8cab4df2b9b35aa1
Gulnaz Sheikh <gulnazsheikh.dev@gmail.com>
2025-10-31 16:08:10 +0530

    add property code done

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,10 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  basePath: isProd ? "/" : "",
-  assetPrefix: isProd ? "//" : "",
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // output: "export",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/" : "",
-  },
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
   sassOptions: {
     includePaths: [path.join(__dirname, "node_modules")],
   },
